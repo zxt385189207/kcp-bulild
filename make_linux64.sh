6c34cbca1daeec1261a4e507ec52e70686ec5891
TianDao <385189207@qq.com>
2020-12-10 17:24:51 +0800

    * 修改make_linux64.sh生成名称 * x86_86/sh/libkcp.so 是使用make_linux64.sh生成的 * x86_64/libkcp.so 是 g++ kcp.c -shared -fPIC -o libkcp.so 命令编译生成

--- make_linux64.sh
+++ make_linux64.sh
@@ -2,5 +2,5 @@ mkdir -p build_linux64 && cd build_linux64
 cmake ../
 cd ..
 cmake --build build_linux64 --config Release
-cp build_linux64/kcp.so Plugins/x86_64/kcp.so
+cp build_linux64/libkcp.so Plugins/x86_64/libkcp.so
 rm -rf build_linux64
